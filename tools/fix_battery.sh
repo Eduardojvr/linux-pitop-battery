7b1f5124eb15e38da1ba5cc333afa2391fb14352
Christopher Clements <bcnjr5@gmail.com>
2017-03-05 14:00:42 -0500

    Added more development tools

---
+++ tools/fix_battery.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# It would be nice if pt-battery-fw-update was documented...
+
+# Check root
+if [ $EUID != 0 ]; then
+  echo 'Run as root.'
+  exit 1
+fi
+
+# Unregister the battery for i2c access
+echo 0x0b > /sys/class/i2c-adapter/i2c-1/delete_device
+
+passes=20
+
+while [ $passes -gt 0 ]; do
+  until ./pt-battery-fw-update -d
+  do
+    echo '=== FAIL ============================================================='
+  done
+  echo '=== SUCCESS =========================================================='
+  let passes--
+  echo "$passes remaining"
+done
+
+echo 'Reboot before it breaks again!'
