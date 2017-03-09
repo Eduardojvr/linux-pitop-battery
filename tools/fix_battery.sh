549dd0c28d1f44a1777f6a0614558457ac03d19a
Christopher Clements <bcnjr5@gmail.com>
2017-03-09 13:19:00 -0500

    Made tools/fix_battery.sh more intelligent

--- tools/fix_battery.sh
+++ tools/fix_battery.sh
@@ -8,7 +8,9 @@ if [ $EUID != 0 ]; then
 fi
 
 # Unregister the battery for i2c access
-echo 0x0b > /sys/class/i2c-adapter/i2c-1/delete_device
+if [ -a /sys/class/i2c-adapter/i2c-1/1-000b ]; then
+  echo 0x0b > /sys/class/i2c-adapter/i2c-1/delete_device
+fi
 
 # This is required by pt-battery-fw-update
 modprobe i2c-dev
